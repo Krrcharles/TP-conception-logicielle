4697027bdf35513d717d1f36ccc80221d9520b35
charles <charleswcarrere@gmail.com>
2024-01-22 16:10:28 +0100

    format for package

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from datetime import datetime
-
+import logging
 import pytz
-
 
 
 # Define the timezone for Réunion
 
-reunion_tz = pytz.timezone('Indian/Reunion')
+logging.basicConfig(filename='log.txt', encoding='utf-8', level=logging.DEBUG)
 
+def get_time():
+    logging.info(f"Lancement du traitement")
+    
+    reunion_tz = pytz.timezone('Indian/Reunion')
 
+    # Get the current time in Réunion
+    current_time_reunion = datetime.now(reunion_tz).strftime('%H:%M:%S')
 
-# Get the current time in Réunion
+    print(f"Il est {current_time_reunion} à la Réunion")
 
-current_time_reunion = datetime.now(reunion_tz)
-
-current_time_reunion = current_time_reunion.strftime('%H:%M:%S')
-
-print(f"Il est {current_time_reunion} à la Réunion")
+if __name__ == "__main__":
+    get_time()
